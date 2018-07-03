ee38124db101185a85add5544fde0f12d8b0320b
aluno <aluno@ad.ifalcvic.local>
2018-07-03 16:23:58 -0300

    primeira importação

---
+++ Exercicios/src/IngressoVIP.java
@@ -0,0 +1,17 @@
+public class IngressoVIP extends Ingresso {
+
+	private int ValorAdicional;
+
+	public void imprimirValorVIP() {
+
+	}
+
+	public int getValorAdicional() {
+		return ValorAdicional;
+	}
+
+	public void setValorAdicional(int valorAdicional) {
+		ValorAdicional = valorAdicional;
+	}
+
+}
